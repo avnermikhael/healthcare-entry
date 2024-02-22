7803eae1a10d00d1af500cfcf8e896c4000c21a0
Avner <mikhael.avner@radyalabs.id>
2024-02-22 11:44:09 +0700

    npm fail start

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'healthcare'
+// })
+
+// app.get('/', (req, res) => {
+//     const sql = "SELECT * FROM patients";
+//     db.query(sql, (err, result) => {
+//         if(err) return res.json({Message : "Error inside server"});
+//         return res.json(result)
+//     })
+// })
+
+app.listen(8081, ()=> {
+    console.log("Listening...")
+})
+
